a83aca93afa11fbc0f526ff85da3b36c52769ec7
unknown <carsonh@gmail.com>
2022-07-10 00:26:24 -0400

    QuerySelectorAll not working

--- aboutus.js
+++ aboutus.js
@@ -1,16 +1,16 @@
-const imagesam = document.querySelectorAll(".imagesam img");
+const images = document.querySelectorAll(".aboutmeimg");
 const modal = document.querySelector(".modal");
 const modalImg = document.querySelector(".modalImg");
 const modalTxt = document.querySelector(".modalTxt");
-const closeam = document.querySelector(".closeam");
+const close = document.querySelector(".close");
 
-imagesam.forEach((image) => {
+images.forEach((image) => {
   image.addEventListener("click", () => {
     modalImg.src = image.src;
     modalTxt.innerHTML = image.alt;
     modal.classList.add("appear");
 
-    closeam.addEventListener("click", () => {
+    close.addEventListener("click", () => {
       modal.classList.remove("appear");
     });
   });
