49758cdf5b29abaa50f3f98e9cf6f56b54a42e05
Carson <carsonh@gmail.com>
2022-07-21 18:45:27 -0400

    Map Directions finished

--- contact.php
+++ contact.php
@@ -59,8 +59,11 @@ if($_POST) {
       
     if(mail($recipient, $email_title, $email_body, $headers)) {
         echo "<p>Thank you for contacting us, $visitor_name. You will get a reply within 24 hours.</p>";
+        echo '<p>Please click the logo below to return to the main menu.</p>';
+        
     } else {
         echo '<p>We are sorry but the email did not go through.</p>';
+        echo '<p>Please click the logo below to return to the main menu.</p>';
     }
       
 } else {
